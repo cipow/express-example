29f199114a55739cf3e5e6f9d02636f727da5d89
cipow <cipoghost@gmail.com>
2017-09-10 19:09:16 +0700

    add gambar/index

--- routes/gambar.js
+++ routes/gambar.js
@@ -5,6 +5,8 @@ var anggota = require('../models/anggota');
 var fs = require('fs');
 var formidable = require('formidable');
 
-
+router.get('/', Auth.is_login, Auth.is_anggota, function(req, res, next) {
+  res.render('gambar/index');
+});
 
 module.exports = router;
